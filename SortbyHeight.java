9784a4f65b7a8b68b69c1f2e6e85c9eccebe6f60
Varsha Sri <varshanaidu.balla0905@gmail.com>
2026-01-29 10:30:50 +0530

    Add files via upload

---
+++ SortbyHeight.java
@@ -0,0 +1,16 @@
+ public class SortbyHeight {
+    public String[] sortPeople(String[] names, int[] heights) {
+        
+        int n = names.length;
+        Integer[] index = new Integer[n];
+        for(int i=0;i<n;i++){
+            index[i] = i;
+        }
+        Arrays.sort(index,(a,b) -> heights[b] - heights[a]);
+        String[] result = new String[n];
+        for(int i= 0 ;i< n;i++){
+            result[i] = names [index[i]];
+        }
+        return result;
+    }
+}
